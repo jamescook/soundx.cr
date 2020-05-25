9230a6739f2a6b60b6e2273046d1100ce99841db
James Cook <jcook.rubyist@gmail.com>
2020-05-25 10:16:28 -0500

    Fix various bugs especially around consecutive soundex codes for different letters

--- spec/soundx_spec.cr
+++ spec/soundx_spec.cr
@@ -3,12 +3,24 @@ require "text"
 
 describe Soundx do
   describe "#encode" do
+    it "returns the correct code for 'Al'" do
+      Soundx.encode("Al").should eq "A400"
+    end
+
+    it "returns the correct code for 'Pfister'" do
+      Soundx.encode("Pfister").should eq "P236"
+    end
+
+    it "returns the correct code for 'jackson'" do
+      Soundx.encode("jackson").should eq "J250"
+    end
+
     it "returns the correct code for 'johnny'" do
       Soundx.encode("johnny").should eq "J500"
     end
 
     it "returns the correct code for 'Schwarzenegger'" do
-      Soundx.encode("Schwarzenegger").should eq "S262"
+      Soundx.encode("Schwarzenegger").should eq "S625"
     end
 
     it "returns the correct code for 'Lukasiewicz'" do
@@ -21,6 +33,18 @@ describe Soundx do
 
     it "returns the correct code for 'rupert'" do
       Soundx.encode("rupert").should eq "R163"
+    end
+
+    it "returns the correct code for 'Ashcraft'" do
+      Soundx.encode("Ashcraft").should eq "A261"
+    end
+
+    it "returns the correct code for 'ashcroft'" do
+      Soundx.encode("ashcroft").should eq "A261"
+    end
+
+    it "returns the correct code for 'Lloyd'" do
+      Soundx.encode("Lloyd").should eq "L300"
     end
   end
 end
--- src/soundx.cr
+++ src/soundx.cr
@@ -3,52 +3,72 @@ module Soundx
   OUTPUT_LENGTH = 4
 
   def self.encode(string : String)
-    buffer = IO::Memory.new(OUTPUT_LENGTH)
-    if string[0].ord > 95 # Conditional upcase
-      buffer << (string[0].ord - 32).chr
-    else
-      buffer << string[0]
-    end
+    String.build do |buffer|
+      i, writes, skip_next_soundex_code, last_code = 0, 0, 0, 0
+      len = string.size
 
-    i = 1
-    writes = 1
-    len = string.size
+      loop do
+        break if writes == OUTPUT_LENGTH || i >= len
+        if i > 0 && string[i].downcase == string[i-1].downcase # Skip consecutive, duplicate characters
+          i += 1
+          next
+        end
 
-    loop do
-      break if writes == OUTPUT_LENGTH || i >= len
-      if i > 1 && string[i] == string[i-1] # Skip consecutive, duplicate characters
+        case string[i].upcase # a Char type
+        when 'H', 'W' # Special handling for H and W per archives.org
+          skip_next_soundex_code = last_code
+        when 'B', 'F', 'P', 'V'
+          unless skip_next_soundex_code == 1
+            buffer << (i == 0 ? string[i].upcase : '1')
+            skip_next_soundex_code = last_code = 1
+            writes += 1
+          end
+        when 'C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z'
+          unless skip_next_soundex_code == 2
+            buffer << (i == 0 ? string[i].upcase : '2')
+            skip_next_soundex_code = last_code = 2
+            writes += 1
+          end
+        when 'D', 'T'
+          unless skip_next_soundex_code == 3
+            buffer << (i == 0 ? string[i].upcase : '3')
+            skip_next_soundex_code = last_code = 3
+            writes += 1
+          end
+        when 'L'
+          unless skip_next_soundex_code == 4
+            buffer << (i == 0 ? string[i].upcase : '4')
+            skip_next_soundex_code = last_code = 4
+            writes += 1
+          end
+        when 'M', 'N'
+          unless skip_next_soundex_code == 5
+            buffer << (i == 0 ? string[i].upcase : '5')
+            skip_next_soundex_code = last_code = 5
+            writes += 1
+          end
+        when 'R', 'r'
+          unless skip_next_soundex_code == 6
+            buffer << (i == 0 ? string[i].upcase : '6')
+            skip_next_soundex_code = last_code = 6
+            writes += 1
+          end
+        else
+          # Ensure we handle vowels if they are the first letter
+          if i == 0
+            buffer << string[i].upcase
+            writes += 1
+          end
+          # Reset
+          skip_next_soundex_code = 0
+        end
         i += 1
-        next
       end
 
-      case string[i] # a Char type
-      when 'B', 'P', 'V', 'b', 'p', 'v'
-        buffer << 1
-        writes += 1
-      when 'C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z', 'c', 'g', 'j', 'k', 'q', 's', 'x', 'z'
-        buffer << 2
-        writes += 1
-      when 'D', 'T', 'd', 't'
-        buffer << 3
-        writes += 1
-      when 'L', 'l'
-        buffer << 4
-        writes += 1
-      when 'M', 'N', 'm', 'n'
-        buffer << 5
-        writes += 1
-      when 'R', 'r'
-        buffer << 6
+      until writes == OUTPUT_LENGTH
+        buffer << '0'
         writes += 1
       end
-      i += 1
     end
-
-    until writes == OUTPUT_LENGTH
-      buffer << '0'
-      writes += 1
-    end
-
-    buffer.to_s
   end
 end
